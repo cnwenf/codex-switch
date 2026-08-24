91db0f8c87e1e76236ec307ba506d4b3a1b2e043
cnwenf <wenfeng.wf@alibaba-inc.com>
2026-08-24 21:07:21 +0800

    test: add fresh-install provider default regression

---
+++ test/default-config.test.js
@@ -0,0 +1,10 @@
+import assert from 'node:assert/strict';
+import fs from 'node:fs';
+import test from 'node:test';
+import TOML from '@iarna/toml';
+
+test('fresh config contains only the ChatGPT subscription provider', () => {
+  const config = TOML.parse(fs.readFileSync(new URL('../config.toml', import.meta.url), 'utf8'));
+  assert.deepEqual(config.providers.map((provider) => provider.id), ['chatgpt-sub']);
+  assert.equal(config.providers.some((provider) => provider.id === 'bailian'), false);
+});
